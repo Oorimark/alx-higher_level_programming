8068dd301b697e6219530100c411b6901ad18606
Oori Mark <oorimark@gmail.com>
2022-09-28 12:21:46 -0700

    Fixe search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,5 +2,5 @@
 
 def search_replace(my_list, search, replace):
     return list(
-                map(lambda x: 89 if x == search else x, my_list)
+                map(lambda x: replace if x == search else x, my_list)
              )
