6a05687e8633a2684a5525173c18cc0829a338cd
Oori Mark <oorimark@gmail.com>
2022-10-10 23:39:58 -0700

    fix 0x00

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,11 +4,11 @@ def safe_print_list(my_list=[], x=0):
     try:
         for i in range(x):
             if i < x:
-                print(my_list[i], end="")
+                print("{:d}".format(my_list[i]), end="")
             else:
                 break
         print("")
         return i + 1
-    except IndexError as err:
+    except Exception as err:
         print("")
         return i
