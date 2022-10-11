0f62566475a610b81a9f872e5faa91b778b10095
Oori Mark <oorimark@gmail.com>
2022-10-10 23:33:13 -0700

    fixed 0x00

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,8 +7,8 @@ def safe_print_list(my_list=[], x=0):
                 print(my_list[i], end="")
             else:
                 break
-        print("\n")
+        print("")
         return i + 1
     except IndexError as err:
-        print("\n")
+        print("")
         return i
