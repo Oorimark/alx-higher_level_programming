4ed1ec098805690e3d66d39669d538d3a3840015
Oori Mark <oorimark@gmail.com>
2022-10-10 23:26:48 -0700

    Prints only integer and show result even if there's an error

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    try:
+        for i in range(x):
+            if i < x:
+                print(my_list[i], end="")
+            else:
+                break
+        print("\n")
+        return i + 1
+    except IndexError as err:
+        print("\n")
+        return i
