65d147b44fbdf9be01707728d31ae6889946a732
Oori Mark <oorimark@gmail.com>
2022-10-11 05:55:55 -0700

    0x00

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
+    counter = 0
+
     try:
         for i in range(x):
             if i < x:
-                print("{:d}".format(my_list[i]), end="")
+                try:
+                    print("{:d}".format(my_list[i]), end="")
+                    counter += 1
+                except Exception:
+                    continue
             else:
                 break
         print("")
-        return i + 1
+        return counter
     except Exception as err:
         print("")
-        return i
+        return counter
