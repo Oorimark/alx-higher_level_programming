5f47a6663f2207ffb74ad105b68eff27a494d5cc
Oori Mark <oorimark@gmail.com>
2022-10-12 21:53:43 -0700

    class with an instance variable

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+class Square:
+    """ A class square with private instance attribute
+
+        Attributes:
+    """
+    def __init__(self, size):
+        """ docstring on __init__ method
+
+            Args:
+                size (int): size of the square
+        """
+        self.__size = size
